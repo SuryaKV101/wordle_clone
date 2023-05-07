cff35d222c359e63c04a8f1c5a25799b4d80d710
Surya KV <kvsuryapersonal@gmail.com>
2023-05-07 15:15:08 -0400

    fixed visual error with the box

---
+++ test.py
@@ -0,0 +1,10 @@
+width, height = input("Enter width and height: ").split()
+width = int(width)
+height = int(height)
+
+print(width, height)
+
+for i in range(0, height):  # range(0, height) -> 0, 1, 2, 3
+    for j in range(0, width):  # range -> 0, 1, 2,...., 7
+        print("*", end=" ")
+    print()
